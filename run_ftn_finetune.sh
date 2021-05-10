c79800675cfe34d0a7137c09b4ea6831e6af2d89
danielafrimi <danielafrimi8@gmail.com>
2021-05-10 13:08:41 +0300

    readme

---
+++ run_ftn_finetune.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --mail-user=daniel.afrimi@mail.huji.ac.il
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --gres=gpu:1,vmem:3g
+#SBATCH --killable
+#SBATCH --mem=3g
+#SBATCH --time=7-0
+#SBATCH -c2
+#SBATCH --output=/cs/labs/werman/daniel023/Lab_vision/model_logs/sbatch_%J.out
+
+module load cuda
+module load torch
+
+source /cs/labs/werman/daniel023/lab_env/bin/activate
+cd /cs/labs/werman/daniel023/Lab_vision/FTN
+
+python3 FinetuneModel.py
+
